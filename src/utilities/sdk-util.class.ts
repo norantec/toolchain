8b13137146b16f2ee360cbe584a8fc18336f152d
lenconda <i@lenconda.top>
2025-06-13 10:57:35 +0800

    fix: 🐛 ignoreCache logic issues

--- src/utilities/sdk-util.class.ts
+++ src/utilities/sdk-util.class.ts
@@ -229,7 +229,7 @@ export class SDKUtil {
             '    }',
             `\n    public async request<T extends keyof ${METHOD_TYPE_MAP_NAME}>(url: T, requestBody?: ${requestBodyTypeAnnotation}, options?: ${REQUEST_OPTIONS_NAME}): Promise<${responseDataTypeAnnotation}> {`,
             '        const requestHash = hash(requestBody ?? null);',
-            '        if (this.RESPONSE_CACHE_MAP.has(requestHash) && !options?.ignoreCache && !this.options?.ignoreCache) {',
+            '        if (this.RESPONSE_CACHE_MAP.has(requestHash) && (options?.ignoreCache === true || this.options?.ignoreCache === true)) {',
             `            return this.RESPONSE_CACHE_MAP.get(requestHash) as ${responseDataTypeAnnotation};`,
             '        }',
             '        const { getAuthorizationCredential, onResponse, ...axiosOptions } = this?.options;',
