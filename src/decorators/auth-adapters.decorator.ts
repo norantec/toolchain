7791fb2b9d372c1c560ac4569b8fbb474f17a74d
lenconda <i@lenconda.top>
2025-06-17 19:58:17 +0800

    feat: 🎸 auth-adapter

--- src/decorators/auth-adapters.decorator.ts
+++ src/decorators/auth-adapters.decorator.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
 import { AuthAdapter } from '../abstract-classes/auth-adapter.abstract.class';
+import { Constructor } from 'type-fest';
 
 export const AUTH_ADAPTERS = Symbol();
 
-export const AuthAdapters = (adapters?: AuthAdapter[]): PropertyDecorator => {
+export const AuthAdapters = (adapters?: Constructor<AuthAdapter>[]): PropertyDecorator => {
     return (target, propertyKey) => {
         Reflect.defineMetadata(AUTH_ADAPTERS, Array.isArray(adapters) ? adapters : [], target, propertyKey);
     };
