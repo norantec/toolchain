3500fdc5ee6e65c3ab92e0848e7b5c35a9ab2caa
lenconda <i@lenconda.top>
2025-06-20 09:52:26 +0800

    fix: 🐛 controller

--- src/basics/controller.ts
+++ src/basics/controller.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
-import { Body, NotFoundException, Post, Req } from '@nestjs/common';
+import { BadRequestException, Body, NotFoundException, Post, Req } from '@nestjs/common';
 import { HeaderUtil } from '@open-norantec/utilities/dist/header-util.class';
-import { z, ZodAny } from 'zod';
+import { z, ZodAny, ZodError } from 'zod';
 import { RequestWithExtraContext } from '../types/request-with-extra-context.type';
 import { HttpResponseBody } from '../interfaces/http-response-body.interface';
 import { StringUtil } from '../utilities/string-util.class';
+import * as _ from 'lodash';
 
 export type MethodHandler<IS extends z.Schema<any>, OS extends z.Schema<any>> = (
     request: RequestWithExtraContext,
@@ -23,8 +24,19 @@ export class Controller {
         ) => Promise<z.infer<OS>>,
     ): MethodHandler<IS, OS> => {
         return async (request, rawInput, headers) => {
-            const input = inputSchema instanceof ZodAny ? rawInput : inputSchema.parse(rawInput);
+            const input = inputSchema instanceof ZodAny ? rawInput : _.attempt(() => inputSchema.parse(rawInput));
+
+            if (input instanceof Error) {
+                if (input instanceof ZodError) {
+                    throw new BadRequestException({
+                        invalidParams: input?.issues?.map?.((item) => item?.path?.join?.('.')) ?? [],
+                    });
+                }
+                throw input;
+            }
+
             const responseData = await callback(input, headers, request);
+
             return {
                 request: input,
                 response: outputSchema instanceof ZodAny ? responseData : outputSchema.parse(responseData),
